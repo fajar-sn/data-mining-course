adf6dc2c380b73b6e8e94f44a62c79ea2a72db6d
Fajar Septian Nugraha <fajarsn99@gmail.com>
2021-05-30 06:01:10 +0700

    Finished association rule task

--- m11_association_rule.py
+++ m11_association_rule.py
@@ -1,5 +1,16 @@
 import pandas
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 
 dataset = pandas.read_csv("assets/transaction.csv")
+data = dataset[dataset["Country"] == "Portugal"]
+transaction = data.groupby(["InvoiceNo", "StockCode"])["Qty"].sum()
+transaction = transaction.unstack().reset_index().fillna(0).set_index("InvoiceNo")
+transaction[transaction > 0] = 1
 
-print(dataset)
+print('\nTabel Transaksi:\n', transaction)
+
+frequent_itemsets = apriori(transaction, min_support=0.2, use_colnames=True)
+rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.7)
+
+print("\nAssociation rules:\n", rules[["antecedents", "consequents", "confidence"]])
